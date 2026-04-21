301d0eb83951a727d868c081c20312b43dec9434
Oleg Mrynskyi <omrynskyi@gmail.com>
2026-04-21 11:41:00 -0700

    Initial Commit

---
+++ Lockit/LockitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockitApp.swift
+//  Lockit
+//
+//  Created by Oleg Mrynskyi on 4/21/26.
+//
+
+import SwiftUI
+
+@main
+struct LockitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
